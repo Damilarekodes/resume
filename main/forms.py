e308d8a44a555337e831d34d6ea9aa5f8a09d4f9
Damilare <osameji42@gmail.com>
2022-06-22 12:37:57 -0700

    initial commit

---
+++ main/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from .models import ContactProfile
+
+
+class ContactForm(forms.ModelForm):
+
+	name = forms.CharField(max_length=100, required=True,
+		widget=forms.TextInput(attrs={
+			'placeholder': '*Full name..',
+			'class': 'field name'
+			}))
+	email = forms.EmailField(max_length=254, required=True, 
+		widget=forms.TextInput(attrs={
+			'placeholder': '*Email..',
+			'class': 'field email'
+			}))
+	message = forms.CharField(max_length=1000, required=True, 
+		widget=forms.Textarea(attrs={
+			'placeholder': '*Message..',
+			'class': 'field textarea',
+			'rows': 6,
+			}))
+
+
+	class Meta:
+		model = ContactProfile
+		fields = ('name', 'email', 'message',)
